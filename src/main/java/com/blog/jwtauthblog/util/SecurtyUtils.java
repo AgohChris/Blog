4eb866b3e4607307f52573fe8b5ab6287d9e8fd5
AgohChris <agihchris90@gmail.com>
2025-08-21 11:36:49 +0000

    Service commentaire et Article

---
+++ src/main/java/com/blog/jwtauthblog/util/SecurtyUtils.java
@@ -0,0 +1,31 @@
+package com.blog.jwtauthblog.util;
+
+import com.blog.jwtauthblog.entity.User;
+import org.springframework.security.core.Authentication
+import org.springframework.security.core.context.SecurityContextHolder;
+
+public class SecurtyUtils {
+    public User getCurrentUser(){
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+
+        if (authentication == null || !authentication.isAuthenticated()){
+            throw new RuntimeException("Aucun utilisateur authentifié");
+        }
+        return (User) authentication.getPrincipal();
+    }
+
+    public boolean hasRole(String roleName){
+        User currentUser = getCurrentUser();
+        return  currentUser.getAuthorities().stream()
+                .anyMatch(auth -> auth.getAuthority().equals("ROLE_" + roleName));
+    }
+
+    public boolean isOwner(Long userId){
+        User currentUser = getCurrentUser();
+        return currentUser.getId().equals(userId);
+    }
+
+    public boolean isOwnerOrAdmin(Long userId){
+        return isOwner(userId) || hasRole("ADMIN");
+    }
+}
